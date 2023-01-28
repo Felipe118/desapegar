68ff0423cd7b7f51e62df1b67da73023b43eade1
Felipe118 <lipebezerraa@gmail.com>
2023-01-28 17:36:55 -0300

    feat: montando a estrutura para iniciar o projeto

---
+++ src/layout/BaseTemplate.vue
@@ -0,0 +1,23 @@
+<template>
+    <header-component></header-component>
+    <section>
+        <router-view></router-view>
+    </section>
+
+    <footer-component></footer-component>
+</template>
+
+<script>
+import HeaderComponent from './components/HeaderComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
+
+
+export default {
+    name: 'BaseTemplate',
+
+    components: {
+        HeaderComponent,
+        FooterComponent
+    }
+}
+</script>
